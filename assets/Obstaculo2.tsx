be70cd8f5a0cff5420d9da362880092e56423578
agomper0806 <algope1990@gmail.com>
2022-12-03 14:03:31 +0100

    Cambios

---
+++ assets/Obstaculo2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Obstaculo2" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="Obstaculo2.png" width="32" height="32"/>
+</tileset>
